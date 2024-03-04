5dc038148c7ff71d66b7a69a03c0bee9bae0437a
yelo-o <flyordig@gmail.com>
2024-03-04 22:26:47 +0900

    #2.4 Hydration

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,9 +2,11 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
     const path = usePathname();
+    const [like, setLike] = useState(0);
     console.log(path);
     return (
     <nav>
@@ -14,6 +16,9 @@ export default function Navigation() {
             </li>
             <li>
                 <Link href="/about-us">About us</Link> {path === "/about-us" ? "🌈" : ""}
+            </li>
+            <li>like : &nbsp;
+                <button onClick={() => setLike((l) => l + 1)}>{like}</button>
             </li>
         </ul>
     </nav>
