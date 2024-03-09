835ad7006e563798cd1f3ca7070b25ba7fcbf035
yelo-o <flyordig@gmail.com>
2024-03-09 15:03:18 +0900

    Server-side data fetching
    The signal of loading can be seen on favicon(refresh) and cannot seen in the client page.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,20 @@
-'use client';
-import { useEffect, useState } from "react";
 
-export default function Page() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState();
-    const getMovies = async () => {
-        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-        const json = await response.json();
-        setMovies(json);
-        setIsLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    })
+export const metadata = {
+    title:"Home",
+}
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMoives() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+export default async function HomePage() {
+    const movies = await getMoives();
     return (
         <div>
-            {isLoading ? "Loading..." : JSON.stringify(movies)};
+            {JSON.stringify(movies)}
         </div>
     );
 }
