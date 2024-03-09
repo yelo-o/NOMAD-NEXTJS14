141235afe935477eea5fe2931f788d6ca9caccc0
yelo-o <flyordig@gmail.com>
2024-03-09 20:03:44 +0900

    #3.4 Parallel Requests
    getVideos() function is executed after getMovie() function exectued in movie detail page. so it will be improved next commit by using Promise.all()

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,26 @@
+import Link from "next/link";
 
 export const metadata = {
     title:"Home",
 }
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMoives() {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(URL);
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
+
 export default async function HomePage() {
     const movies = await getMoives();
     return (
         <div>
-            {JSON.stringify(movies)}
+            {movies.map(movie => (
+            <li key={movie.id}>
+                <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+            </li>
+            ))}
         </div>
     );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,3 +1,27 @@
-export default function MovieDetail({params: {id}} : {params:{id:string}}) {
-    return <h1>Movie {id}</h1>;
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id:string) {
+    console.log(`Fetching movies: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id:string) {
+    console.log(`Fetching videos: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail({
+    params: { id }
+} : {
+    params:{ id:string }
+}) {
+    console.log('start fetching')
+    const movie = await getMovie(id);
+    const videos = await getVideos(id);
+    console.log('end fetching')
+    return <h1>{movie.title}</h1>;
 }
