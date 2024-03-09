7c4c0b43a89156f97fac5d95d438833d79cc2027
yelo-o <flyordig@gmail.com>
2024-03-09 20:09:55 +0900

    use Promise.all() to make it exectued simultaneously

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -20,8 +20,9 @@ export default async function MovieDetail({
     params:{ id:string }
 }) {
     console.log('start fetching')
-    const movie = await getMovie(id);
-    const videos = await getVideos(id);
+    // const movie = await getMovie(id);
+    // const videos = await getVideos(id);
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
     console.log('end fetching')
     return <h1>{movie.title}</h1>;
 }
