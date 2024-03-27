37616a9d11628bca78ed4ea43a80c004cb67d905
yelo-o <flyordig@gmail.com>
2024-03-27 20:42:18 +0900

    apply navigation css styles by using navigation.module.css

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,23 +2,18 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
     const path = usePathname();
-    const [like, setLike] = useState(0);
-    console.log(path);
     return (
-    <nav>
+    <nav className={styles.nav}>
         <ul>
             <li>
                 <Link href="/">Home</Link> {path === "/" ? "🌈" : ""}
             </li>
             <li>
                 <Link href="/about-us">About us</Link> {path === "/about-us" ? "🌈" : ""}
-            </li>
-            <li>like : &nbsp;
-                <button onClick={() => setLike((l) => l + 1)}>{like}</button>
             </li>
         </ul>
     </nav>
