898bbff64ff8d6bdc9add8c0a1a7f7a5af0b7ae5
yelo-o <flyordig@gmail.com>
2024-03-28 21:30:09 +0900

    fix impulsive exception for see the data

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,9 +3,9 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id:string) {
     console.log(`Fetching videos: ${Date.now()}`);
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    throw new Error('something broke...');
-    // const response = await fetch(`${API_URL}/${id}/videos`);
-    // return response.json();
+    // throw new Error('something broke...');
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
 }
 
 export default async function MovieVideos({id}:{id:string}) {
