f65f48c8ef969e4bc089405ae67646298ab856c5
yelo-o <flyordig@gmail.com>
2024-03-29 23:57:41 +0900

    add movie-info style css

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,3 +1,4 @@
+import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id:string) {
@@ -9,5 +10,12 @@ async function getMovie(id:string) {
 
 export default async function MovieInfo({id}:{id:string}) {
     const movie = await getMovie(id);
-    return <h6>{JSON.stringify(movie)}</h6>
+    return <div className={potato.container} >
+        <img src={movie.poster_path} className={potato.poster} alt={movie.title}/>
+        <div className={potato.info}>
+            <h1 className={potato.title}>{movie.title}</h1>
+            <h3>🌟{movie.vote_average}</h3>
+            <p>{movie.overview}</p>
+        </div>
+    </div>
 }
