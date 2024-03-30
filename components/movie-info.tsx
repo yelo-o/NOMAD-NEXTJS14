6741a9f36bf6e7597b3a93821ef0a1ecbf5f1134
yelo-o <flyordig@gmail.com>
2024-03-30 12:32:30 +0900

    #4.4 Dynamic Metadata : get movie title for every single page

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,9 +1,9 @@
 import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id:string) {
+export async function getMovie(id:string) {
     console.log(`Fetching movies: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,7 +3,7 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id:string) {
     console.log(`Fetching videos: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     // throw new Error('something broke...');
     const response = await fetch(`${API_URL}/${id}/videos`);
     return response.json();
